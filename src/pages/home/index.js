7163817e666a767ad06c603bb91c176417c9f920
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-06-27 12:23:58 -0300

    Button

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import Button from '../../components/button'
 const home = () => {
   return (
     <div>
         <h1>Home</h1>
       <Link to="/login">Ir para login</Link>
-        
+        <Button/>
+        <Button variant="secondary"/>
         </div>
   )
 }
