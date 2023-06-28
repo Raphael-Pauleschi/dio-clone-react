8433dee77f9696e29e6ef7fbaa3010795ac761c7
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-06-28 15:39:58 -0300

    Login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Header from '../../components/header'
+import React from "react";
+import Header from "../../components/header";
+import {
+  Container,
+  Title,
+  TitleLogin,
+  SubTitleLogin,
+  ForgotText,
+  CreateText,
+  Row,
+  Column,
+  Wrapper,
+} from "./styles";
+import Button from "../../components/button";
+import Input from "../../components/input";
 
 const login = () => {
   return (
-    <Header/>
-  )
-}
+    <>
+      <Header />
+      <Container>
+        <Column>
+          <Title>
+            A plataforma para você aprender com experts, dominar as principais
+            tecnologias e entrar mais rápido nas empresas mais desejadas{" "}
+          </Title>
+        </Column>
+        <Column>
+          <Wrapper>
+            <TitleLogin>Faça o seu cadastro</TitleLogin>
+            <SubTitleLogin>Faça seu login e make the change</SubTitleLogin>
+            <form>
+              <Input placeholder="E-mail" />
+              <Input placeholder="Senha" type="password" />
+              <Button title="entrar" variant="secondary" />
+            </form>
+          </Wrapper>
+          <Row>
+            <ForgotText>Esqueci a minha senha</ForgotText>
+            <CreateText>Criar Conta</CreateText>
+          </Row>
+        </Column>
+      </Container>
+    </>
+  );
+};
 
-export default login
+export default login;
